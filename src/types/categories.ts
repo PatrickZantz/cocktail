d1579c2d43ae5cb5fd6589809beea8d59e1ada77
PatrickZantz <p.zantz@gmail.com>
2025-03-11 12:29:56 -0300

    col

--- src/types/categories.ts
+++ src/types/categories.ts
@@ -1,7 +1,7 @@
 // categories.ts
 export const categories = [
     { name: "Gin", color: "bg-blue-300", description: "Find your favorite cocktail" },
-    { name: "Vodka", color: "bg-violet-400", description: "Find your favorite cocktail" },
+    { name: "Vodka", color: "bg-violet-300", description: "Find your favorite cocktail" },
     { name: "Rum", color: "bg-red-300", description: "Find your favorite cocktail" },
     { name: "Scotch", color: "bg-orange-300", description: "Find your favorite cocktail" },
     { name: "Alkoholfrei", color: "bg-indigo-300", description: "Find your favorite cocktail" },
